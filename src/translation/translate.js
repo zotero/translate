85b39a5be5bb59c5ee5e7447609273a29ea0b799
Dan Stillman <dstillman@zotero.org>
2021-11-25 00:20:51 -0500

    Fix Zotero.RDF.getResourceURI() for nodes with rdf:about and rdf:value
    Return a URI based on the `rdf:about` attribute if present rather than
    the contents of the `rdf:value` element. In particular, this means that
    for an exported note (a `bib:Memo` node), a URI ending in, e.g.,
    `#item_3` will be returned from `getResourceURI()` rather than the note
    cnotents.
    
    This is part of the fix for standalone notes in RDF exports not being
    added to collections they were in on re-import, due to the lack of a
    valid id.
    
    This behavior dates to zotero/zotero#453 from 2014 (which I apparently
    knew broke `getResourceURI()` for notes as of zotero/zotero@9c2d0d727 in
    2019).

--- src/translation/translate.js
+++ src/translation/translate.js
@@ -3083,13 +3083,14 @@ Zotero.Translate.IO._RDFSandbox.prototype = {
 	"getResourceURI":function(resource) {
 		if(typeof(resource) == "string") return resource;
 		
+		if (resource.uri) return resource.uri;
+		
 		const rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 		var values = this.getStatementsMatching(resource, rdf + 'value');
 		if (values && values.length) {
 			return values[0][2];
 		}
 		
-		if(resource.uri) return resource.uri;
 		if(resource.toNT == undefined) throw new Error("Zotero.RDF: getResourceURI called on invalid resource");
 		return resource.toNT();
 	},
